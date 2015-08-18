cd380e14c2ae347033bcd0f92743d0436290f38c
Hobson Lane <hobsonlane@gmail.com>
2015-08-18 13:02:09 -0700

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # FIXME: find all revealjs markdown files and run pandoc on them like this
-pandoc -t revealjs --template=`pwd`/../pandoc-template-for-revealjs.html -V theme=moon -s `pwd`/../hobson.github.io/_posts/2015-08-22-TFNW-BYO-Brain-Workshop.md -o `pwd`/2015-08-22-TFNW-BYO-Brain-Workshop.html
+pandoc -t revealjs --template=`pwd`/pandoc-template-for-revealjs.html -V theme=moon -s `pwd`/../hobson.github.io/_posts/2015-08-22-TFNW-BYO-Brain-Workshop.md -o `pwd`/2015-08-22-TFNW-BYO-Brain-Workshop.html
